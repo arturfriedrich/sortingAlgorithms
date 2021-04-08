4d8a78c9b529ad92b10f117af2a97865b3733e0d
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2021-04-08 21:19:22 +0200

    Created bubbleSortPython-py

---
+++ python/bubbleSortPython.py
@@ -0,0 +1,11 @@
+def bubble_sort(list):
+    for i in range(0, len(a) - 1):
+        for j in range(len(a) - 1):
+            if a[j] > a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
+    return list
+
+
+a = [2, 9, 3, 5, 1, 7, 4]
+print("Original list:", a)
+print("Bubble sorted list:", bubble_sort(a))
