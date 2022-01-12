6d3f3594171e76ba5ae474a5bc377dab971ddf36
Abdulrahman Nasser <68697507+Abdul-coder-maker@users.noreply.github.com>
2022-01-12 12:13:46 +0400

    Update bubbleSortJS.js

--- bubbleSort/bubbleSortJS.js
+++ bubbleSort/bubbleSortJS.js
@@ -1,16 +1,6 @@
 function bubbleSort(a) {
-    for (var i = 0; i < a.length; i++) {
-        for (var j = 0; j < (a.length - i - 1); j++) {
-            if (a[j] > a[j + 1]) {
-                [a[j], a[j + 1]] = [a[j + 1], a[j]]
-                console.log(`${i}. and ${j}. number switched:`, a)
-            }
-        }
-    }
-    return a
+  return a.sort((a, b) => a - b);
 }
 
-var a = [8, 9, 3, 5, 1]
-
-console.log("Original array: " + a)
-console.log("Sorted array: " + bubbleSort(a))
+var a = [8, 9, 3, 5, 1];
+console.log("Sorted array: " + bubbleSort(a));
